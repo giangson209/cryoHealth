a94b92e8d1e66ab516c4c8641c6dae8949e7ebdc
Sơn Lê Đình <ledinhson209@gmail.com>
2022-10-07 03:03:06 +0700

    updaqte 123

--- howwework.php
+++ howwework.php
@@ -1,3 +1,101 @@
 <?php include 'header.php';?>
-<main></main>
-<?php include 'footer.php';?>
+<main>
+	<section class="box-banner-bread">
+		<div class="container">
+			<div class="content-bread text-center">
+				<h1>We create <span>your sport recovery room</span></h1>
+			</div>
+		</div>
+	</section>
+	<section class="box-content-how">
+		<div class="list-recovery-room">
+			<div class="item-recover-room">
+				<div class="container">
+					<div class="item-how">
+						<div class="avarta"><img src="images/how-1.png" class="img-fluid w-100" alt=""></div>
+						<div class="info">
+							<h3>Sport</h3>
+							<div class="desc">
+								<p>Zowel voor de elite en competitieve sporter als voor de gepassioneerde recreant helpen wij je graag met het bereiken van je sportieve doelen en het boosten van je energie. </p>
+								<p>Maar ook voor elke sportclub, professioneel of amateur, bouwen wij samen met jou de ideale herstel ruimte voor elk team.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item-memb-how">
+						<div class="avr-mem"><img src="images/h-1.png" class="img-fluid" alt=""></div>
+						<div class="info">
+							<div class="caption-mem">
+								“Dankzij CryoHealth’s advies is mijn sportherstel optimaal”
+							</div>
+							<p>Birger Verstraete - professional socces player</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item-recover-room">
+				<div class="container">
+					<div class="item-how">
+						<div class="avarta"><img src="images/how-1.png" class="img-fluid w-100" alt=""></div>
+						<div class="info">
+							<h3>Sport</h3>
+							<div class="desc">
+								<p>Zowel voor de elite en competitieve sporter als voor de gepassioneerde recreant helpen wij je graag met het bereiken van je sportieve doelen en het boosten van je energie. </p>
+								<p>Maar ook voor elke sportclub, professioneel of amateur, bouwen wij samen met jou de ideale herstel ruimte voor elk team.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item-memb-how">
+						<div class="avr-mem"><img src="images/h-1.png" class="img-fluid" alt=""></div>
+						<div class="info">
+							<div class="caption-mem">
+								“Dankzij CryoHealth’s advies is mijn sportherstel optimaal”
+							</div>
+							<p>Birger Verstraete - professional socces player</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="item-recover-room">
+				<div class="container">
+					<div class="item-how">
+						<div class="avarta"><img src="images/how-1.png" class="img-fluid w-100" alt=""></div>
+						<div class="info">
+							<h3>Sport</h3>
+							<div class="desc">
+								<p>Zowel voor de elite en competitieve sporter als voor de gepassioneerde recreant helpen wij je graag met het bereiken van je sportieve doelen en het boosten van je energie. </p>
+								<p>Maar ook voor elke sportclub, professioneel of amateur, bouwen wij samen met jou de ideale herstel ruimte voor elk team.</p>
+							</div>
+						</div>
+					</div>
+					<div class="item-memb-how">
+						<div class="avr-mem"><img src="images/h-1.png" class="img-fluid" alt=""></div>
+						<div class="info">
+							<div class="caption-mem">
+								“Dankzij CryoHealth’s advies is mijn sportherstel optimaal”
+							</div>
+							<p>Birger Verstraete - professional socces player</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="bottom-howwork">
+		<div class="container">
+			<div class="desc-content">
+				Dé <span>partner voor iedereen</span> die actief  aan <span>sport</span> doet, een revalidatieproces extra wil ondersteunen of aandacht wil schenken aan zijn of <span>haar algemene welzijn.</span>
+			</div>
+			<div class="btn-bott">
+				<ul>
+					<li>
+						<div class="btn-main text-uppercase"><a href="">BOEK EEN GESPREK</a></div>
+					</li>
+					<li>
+						<div class="btn-main text-uppercase"><a href="">discover our products</a></div>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</section>
+</main>
+<?php include 'footer.php';?> 
